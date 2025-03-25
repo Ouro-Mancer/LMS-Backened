f6eec13470e7add6793b040b999e4f73a152997d
Mayank <mayankkumardas12@gmail.com>
2025-03-25 13:25:00 +0530

    chnage server file

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ import { app } from "./app.js";
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+    console.log(`Server is listening on port:: ${port}`);
 });
